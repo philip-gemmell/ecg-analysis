7a4746b9cdd2ec4ad4f9afff90e47ccd45422e31
Philip Gemmell <philip.gemmell@gmail.com>
2019-12-11 14:47:17 +0000

    Added common_analysis.find_list_fraction
    Created custom function to find (and possibly interpolate) values
    at a given fraction length within a list, rather than just the
    median value given by common_analysis.find_list_middle, e.g. can
    now find value 1/3 of way along list

--- ecg-analysis/common_analysis.py
+++ ecg-analysis/common_analysis.py
@@ -193,6 +193,41 @@ def convert_to_plane(x, y):
         return -r
 
 
+def find_list_fraction(input_list, fraction=0.5, interpolate=True):
+    fraction_list = np.linspace(0, 1, len(input_list))
+    fraction_bounds = 0.1
+    fraction_idx = np.where((fraction_list >= fraction-fraction_bounds) &
+                            (fraction_list <= fraction+fraction_bounds))[0]
+    while len(fraction_idx) > 2:
+        fraction_bounds /= 10
+        fraction_idx = np.where((fraction_list >= fraction - fraction_bounds) &
+                                (fraction_list <= fraction + fraction_bounds))[0]
+
+    if len(fraction_idx) == 1:
+        return input_list[fraction_idx[0]]
+    else:
+        if interpolate:
+            """ Interpolate between two values, based on:
+                l = l_a * f_a + l_b * f_b
+                where l_a is value of list at fraction_idx[0] and l_b is value of list at fraction_idx[1]
+                f_a = (-1/(b-a))*(x-a) + 1
+                f_b = (1/(b-a))*(x-a)
+                where a=fraction_idx[0], b=fraction_idx[1], and x is the actually desired fraction
+            """
+            a = fraction_list[fraction_idx[0]]
+            b = fraction_list[fraction_idx[1]]
+            gradient = 1/(b-a)
+            f_a = -gradient*(fraction-a)+1
+            f_b = gradient*(fraction-a)
+            # Return different answers, depending on whether the input list is a list of lists or not
+            if isinstance(input_list[0], list):
+                return [i*f_a+j*f_b for i, j in zip(input_list[fraction_idx[0]], input_list[fraction_idx[1]])]
+            else:
+                return input_list[fraction_idx[0]]*f_a + input_list[fraction_idx[1]]*f_b
+        else:
+            return tuple(np.array(input_list)[fraction_idx])
+
+
 def find_list_middle(input_list, interpolate=True):
     middle = float(len(input_list))/2
     if middle % 2 != 0:
@@ -201,4 +236,4 @@ def find_list_middle(input_list, interpolate=True):
         if interpolate:
             return np.mean((input_list[int(middle)], input_list[int(middle-1)]), axis=0)
         else:
-            return input_list[int(middle)], input_list[int(middle-1)]
+            return input_list[int(middle-1)], input_list[int(middle)]
